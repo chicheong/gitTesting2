ca23241d29e197b7ee1faf43e8a7aae14d2f188b
Kelvin Wong <kelvin_cc_wong@dh.gov.hk>
2022-08-08 16:02:31 +0800

    change to very bad

--- dental.test.git.third/src/dental/test/git/third/gitTest.java
+++ dental.test.git.third/src/dental/test/git/third/gitTest.java
@@ -2,6 +2,6 @@ package dental.test.git.third;
 
 public class gitTest {
   public static void main(String[] args) {
-	    System.out.println("Git is bad");
+	    System.out.println("Git is very bad");
   }
 }
