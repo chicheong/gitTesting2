d258c2509bd08735b6d0230dc950566dac14f53e
Kelvin Wong <kelvin_cc_wong@dh.gov.hk>
2022-08-08 17:27:46 +0800

    edit the file later than master

--- dental.test.git.third/src/dental/test/git/third/gitTest3.java
+++ dental.test.git.third/src/dental/test/git/third/gitTest3.java
@@ -2,6 +2,6 @@ package dental.test.git.third;
 
 public class gitTest3 {
 	  public static void main(String[] args) {
-		    System.out.println("I started to edit the file");
+		    System.out.println("I edited the file late");
 	  }
 }
