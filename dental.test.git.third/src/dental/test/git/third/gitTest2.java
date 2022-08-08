bdc1dfe210be3ba0c66fd1dc4caa93206cc7287e
chicheong <chicheong@gmail.com>
2022-08-08 16:17:29 +0800

    Update gitTest2.java

--- dental.test.git.third/src/dental/test/git/third/gitTest2.java
+++ dental.test.git.third/src/dental/test/git/third/gitTest2.java
@@ -2,6 +2,6 @@ package dental.test.git.third;
 
 public class gitTest2 {
   public static void main(String[] args) {
-	    System.out.println("Git was good");
+	    System.out.println("Git was good? I doubted");
   }
 }
